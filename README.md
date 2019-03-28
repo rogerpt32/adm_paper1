0f97920e603f5a828142afd8969a6d04072b91d7
rogerpt32 <rogerpt32@gmail.com>
2019-03-28 15:39:02 +0100

    Organize the project and add README

---
+++ README.md
@@ -0,0 +1,5 @@
+#SGEMM Product analysis
+In this project I will try to analyse the optimization of various configurations for SGEMM products.
+##Usage
+##Motivation
+This project is done for the ADM subject from the MIRI of the UPC.
--- src/preprocess.py
+++ src/preprocess.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
-df = pd.read_csv("sgemm_product_dataset/sgemm_product.csv")
+df = pd.read_csv("../data/sgemm_product.csv")
 single_df = df.drop(columns=df.columns[[15,16,17]])
 single_df = single_df.rename(index=str, columns={"Run1 (ms)": "Run2 (ms)"})
 single_df = single_df.append(df.drop(columns=df.columns[[14,16,17]]),ignore_index=True)
@@ -11,7 +11,7 @@ single_df = single_df.append(df.drop(columns=df.columns[[14,15,17]]),ignore_inde
 single_df = single_df.rename(index=str, columns={"Run3 (ms)": "Run4 (ms)"})
 single_df = single_df.append(df.drop(columns=df.columns[[14,15,16]]),ignore_index=True)
 single_df = single_df.rename(index=str, columns={"Run4 (ms)": "Run (ms)"})
-single_df.to_csv('sgemm_preprocessed.csv', sep=',', encoding='utf-8')
+single_df.to_csv('../data/sgemm_preprocessed.csv', sep=',', encoding='utf-8')
 
 pos=1
 for col in single_df.columns[:-1]:
